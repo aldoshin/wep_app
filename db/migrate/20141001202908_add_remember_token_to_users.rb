c3f5d957d707b0e893eba845a8bbe7dc7b25762c
aldoshin <aldoshin@gmail.com>
2014-10-02 16:18:37 -0400

    migratioon comment

--- db/migrate/20141001202908_add_remember_token_to_users.rb
+++ db/migrate/20141001202908_add_remember_token_to_users.rb
@@ -2,5 +2,6 @@ class AddRememberTokenToUsers < ActiveRecord::Migration
   def change
   	add_column :users, :remember_token, :string
   	add_index :users, :remember_token
+  	#adding comment
   end
 end
