025225b67fac67558c3d035c0710259f69a8c2eb
aldoshin <aldoshin@gmail.com>
2014-12-08 16:01:34 -0500

    displaying microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@ describe User do
 	it { should respond_to :password_confirmation }
 	it { should respond_to :remember_token }
 	it { should respond_to(:admin) }
+	it { should respond_to(:microposts) }
 	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
@@ -123,5 +124,28 @@ describe User do
 	describe "remember token" do
 		before { @user.save }
 		its(:remember_token) { should_not be_blank }
+	end
+
+	describe "micropost association" do
+		before { @user.save }
+		let!(:older_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+		end
+		let!(:newer_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+		end
+
+		it "should have the rigth micropost in the rigth order" do
+			expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+		end
+
+		it "should destroy asscoiated microposts" do
+			microposts = @user.microposts.to_a
+			@user.destroy
+			expect(microposts).not_to be_empty
+			microposts.each do |micropost|
+				expect(Micropost.where(id: micropost.id)).to be_empty
+			end
+		end
 	end
 end
